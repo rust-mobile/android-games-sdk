152cf2203d8b30e1749c8809fca14e21efba41c7
Robert Bragg <robert@sixbynine.org>
2025-03-31 20:01:53 +0100

    Don't send (unused) APP_CMD_KEY/TOUCH_EVENTs
    We already have an `InputAvailable` wakeup event to ensure the
    application will poll input within a finite amount of time after new
    input arrives.
    
    The advantage of `InputAvailable` is that it's more consistent with
    `NativeActivity` and it doesn't require us to write an event for _every_
    input event if we know the application has already been notified (and
    has not yet polled for new input).

--- game-activity/prefab-src/modules/game-activity/src/game-activity/native_app_glue/android_native_app_glue.c
+++ game-activity/prefab-src/modules/game-activity/src/game-activity/native_app_glue/android_native_app_glue.c
@@ -579,7 +579,7 @@ static bool onTouchEvent(GameActivity* activity,
   ++inputBuffer->motionEventsCount;
   notifyInput(android_app);
 
-  android_app_write_cmd(android_app, APP_CMD_TOUCH_EVENT);
+  //android_app_write_cmd(android_app, APP_CMD_TOUCH_EVENT);
   pthread_mutex_unlock(&android_app->mutex);
   return true;
 }
@@ -665,7 +665,7 @@ static bool onKey(GameActivity* activity, const GameActivityKeyEvent* event) {
   ++inputBuffer->keyEventsCount;
   notifyInput(android_app);
 
-  android_app_write_cmd(android_app, APP_CMD_KEY_EVENT);
+  //android_app_write_cmd(android_app, APP_CMD_KEY_EVENT);
   pthread_mutex_unlock(&android_app->mutex);
   return true;
 }
